3f5cb8a4add2c9683a9c19c3b73df3096f242d56
Nataliia <nvasinaa@gmail.com>
2022-06-25 10:59:41 +0300

    маша

---
+++ 005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите ваше имя, ");
+string username = Console.ReadLine();
+
+if (username=="маша")
+{
+    Console.WriteLine("ура этоже маша");
+}
+else
+{
+    Console.WriteLine("Привет ");
+    Console.WriteLine(username);
+}
