74e2352ac13cea2a52eed511080a5a71143ba274
Nataliia <nvasinaa@gmail.com>
2022-06-27 21:06:51 +0300

    сложение

---
+++ 011/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите число от 1 до 7");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == 1)
+{
+    Console.WriteLine("понедельник");
+}
+if (number1 == 2)
+{
+    Console.WriteLine("вторник");
+}
+if (number1 == 3)
+{
+    Console.WriteLine("среда");
+}
