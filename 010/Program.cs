95a97b3471c305cb9be8e6c8dbe4dcac43c2584d
Nataliia <nvasinaa@gmail.com>
2022-06-27 21:06:12 +0300

    дни недели

---
+++ 010/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("первое число не является квадратом второго");
+}
