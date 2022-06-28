f257fcf2ce3b66961980ec5f921ec42f2ae6cde3
Nataliia <nvasinaa@gmail.com>
2022-06-28 12:01:34 +0300

    Задача 2 из ДЗ по с#

---
+++ 013/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 24;
+int numberB = 16;
+
+int max = numberA;
+
+if (max > numberB)
+{
+    Console.WriteLine("большее число " + numberA); 
+    Console.WriteLine("меньшее число " + numberB);
+}
+else
+{
+    Console.WriteLine("большее число " + numberB); 
+    Console.WriteLine("меньшее число " + numberA); 
+}
+    
