581eea9bf04fc5afca33fb634c6a411b364735d4
Nataliia <nvasinaa@gmail.com>
2022-06-28 13:08:04 +0300

    Задача 8

---
+++ 016/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = 40;
+int number = 1;
+
+while (number < N)
+{
+    if (number%2 == 0)
+        {
+        Console.WriteLine(number);
+        number++;
+        }
+        else
+        {
+          number++;  
+        }
+}
