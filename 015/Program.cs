e821ed5c7691aeb30f0d0798359adb45894096ed
Nataliia <nvasinaa@gmail.com>
2022-06-28 12:12:13 +0300

    Задача 4

---
+++ 015/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberA = 24;
+int numberB = 16;
+int numberC = 44;
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine("большее число " + max); 
