8e9aa147628b808c4b3f5a5d213f102cde5ff1cd
Nataliia <nvasinaa@gmail.com>
2022-06-29 23:10:28 +0300

    Выходной или нет Задача 15

---
+++ 017/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("введите число соответствующие дню недели");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сейчас проверим выходной ли это!");
+
+if (number1 == 1)
+{
+    Console.WriteLine("нет");
+}
+if (number1 == 2)
+{
+    Console.WriteLine("нет");
+}
+if (number1 == 3)
+{
+    Console.WriteLine("нет");
+}
+if (number1 == 4)
+{
+    Console.WriteLine("нет");
+}
+    if (number1 == 5)
+    {
+       Console.WriteLine("нет");
+    }
+    if (number1 == 6)
+    {
+        Console.WriteLine("да");
+    }
+    if (number1 == 7)
+    {
+        Console.WriteLine("да");
+    }
+  
+
+
