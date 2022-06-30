ee9a68978e55169d725fc7660d751421e1e2d0df
Nataliia <nvasinaa@gmail.com>
2022-06-30 21:53:51 +0300

    021

---
+++ 021/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите X1: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Y1: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите X2: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите Y2: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+
+void Zadacha21(int X1, int Y1, int X2, int Y2)
+{
+    Console.WriteLine(Math.Sqrt((Y2-Y1)*(Y2-Y1) + (X2-X1)*(X2-X1)));
+}
+Zadacha21(X1, Y1, X2, Y2);
