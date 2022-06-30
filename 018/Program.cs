f305258b9a6a0dd4e7ee96d03712cc067bc90091
Nataliia <nvasinaa@gmail.com>
2022-06-30 14:16:55 +0300

    Задача 15

---
+++ 018/Program.cs
@@ -0,0 +1,17 @@
+﻿string number = "24978";
+ NewMethod(number);
+
+        void NewMethod(string currentnumber)
+        {
+
+
+            if (currentnumber.Length < 3)
+            {
+                Console.WriteLine("третьей цифры нет");
+            }
+            else if (currentnumber[2] != 0)
+            {
+                Console.WriteLine(currentnumber[2]);
+            }
+        }
+ 
