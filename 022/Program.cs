3615d45d2398a2b95207525bdaba2e57005b2185
Nataliia <nvasinaa@gmail.com>
2022-06-30 21:54:05 +0300

    022

---
+++ 022/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("введите целое натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Zadacha22(int number)
+{
+   int count = 1;
+   while (count <= number)
+   {
+        Console.WriteLine(count*count);
+      count++;
+   }
+}
+Zadacha22(number);
+
+
+
