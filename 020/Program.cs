ff4227e1200edb08aebe9b94300fe6a67972c4dc
Nataliia <nvasinaa@gmail.com>
2022-06-30 21:53:36 +0300

    020

---
+++ 020/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите четверть: ");
+int i = Convert.ToInt32(Console.ReadLine());
+
+void Zadacha18(int i)
+{
+    if (i == 1)
+    {
+        Console.WriteLine("x (0;infinity), y(0;infinity)");
+    }
+    else if (i == 2)
+    {
+        Console.WriteLine("x (infinity;0), y(0;infinity)");
+    }
+    else if (i == 3)
+    {
+        Console.WriteLine("x (infinity;0), y(infinity;0)");
+    }
+    else if (i == 4)
+    {
+        Console.WriteLine("x (0;infinity), y(infinity;0)");
+    }
+}
+Zadacha18(i);
