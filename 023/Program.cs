e55ba489eb9e76bce027ff62c6df0f02c8d58021
Nataliia <nvasinaa@gmail.com>
2022-07-03 13:28:13 +0300

    Задача 19 на C#

---
+++ 023/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое натуральное пятизначное число: ");
+string number = Console.ReadLine();
+
+void Zadacha19()
+{
+ if (number[0]==number[4] && number[1]==number[3])
+    {
+        Console.WriteLine("является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("не является палиндромом");
+    }
+}
+Zadacha19();
