b4dcd500cdf32485ef5a3529420bcd6e534fcad1
Nataliia <nvasinaa@gmail.com>
2022-07-03 13:57:19 +0300

    Задача 23 на C#

---
+++ 025/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите целое натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Zadacha23(int number)
+{
+   int count = 1;
+   while (count <= number)
+   {
+        Console.WriteLine(count*count*count);
+      count++;
+   }
+}
+Zadacha23(number);
+
